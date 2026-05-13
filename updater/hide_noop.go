9ca0a1faadf0e10b26b107e817fe48772d2107ce
dido18 <davideneri18@gmail.com>
2026-05-13 12:01:07 +0200

    reause

--- updater/hide_noop.go
+++ updater/hide_noop.go
@@ -1,6 +1,5 @@
 // This file is part of go-updater.
 //
-// Copyright 2025 ARDUINO SA (http://www.arduino.cc/)
 // SPDX-FileCopyrightText: Arduino s.r.l. and/or its affiliated companies
 // SPDX-License-Identifier: GPL-3.0-or-later
 
